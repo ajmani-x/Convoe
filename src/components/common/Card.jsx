7e45d45cfb333a209118193125108d124c057e7a
Surya Pratap Singh <106734101+spsi-gh@users.noreply.github.com>
2025-10-22 22:55:36 +0530

    Add Card, CardHeader, and CardBody components

---
+++ src/components/common/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Card = ({ children, className = '' }) => {
+  return (
+    
+      {children}
+    
+  );
+};
+
+export const CardHeader = ({ children, className = '' }) => {
+  return (
+    
+      {children}
+    
+  );
+};
+
+export const CardBody = ({ children, className = '' }) => {
+  return (
+    
+      {children}
+    
+  );
+};
